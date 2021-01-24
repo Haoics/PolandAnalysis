03e9d0dae625f4430f03d7bc57d51caa8e5b3d85
FEDRA IPPOLITO <fedra.ippolito98@gmail.com>
2021-01-24 13:02:46 +0100

    linear regression

---
+++ 02_Linear_Analysis.R
@@ -0,0 +1,13 @@
+#linear analysis of the two variables "trstprl" and "trstep"
+
+options(digits = 4)
+
+linear_analysis <- lm(trstprl ~ trstep,
+                      data = poland_sub_clean)
+summary(linear_analysis)
+
+#plotting linear regression
+
+ggplot(poland_sub_clean, aes(x = trstprl, y = trstep)) +
+  geom_smooth(method = "lm") +
+  theme_bw()
