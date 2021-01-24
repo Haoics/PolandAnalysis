e932135904f9aae309987ab22680250c0c1404e2
simonedeluca <delucasim@hotmail.it>
2021-01-24 12:38:13 +0100

    Data cleaning + distribution

--- 00_DataCleaning.R
+++ 00_DataCleaning.R
@@ -8,10 +8,18 @@ poland_sub$trstprl
 poland_sub_clean <- filter(poland_sub,
                            trstprl != c(77, 88, 99, NA))
 
+count(poland_sub_clean, vars = trstprl)
+
 poland_sub$trstep
 
 poland_sub_clean <- filter(poland_sub,
                            trstep != c(77, 88, 99, NA))
+
+count(poland_sub_clean, vars = trstep)
+
+poland_sub_clean %>%
+  summarize_if(is.numeric, ~mean(., na.rm = T))
+
 
 
 
